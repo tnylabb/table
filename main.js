bacdcdb22071a1ef8e59daff23b6dfda18385f6d
tnylab <aranyi.balint@gmail.com>
2024-10-04 10:05:54 +0200

    Add Person to Array

--- main.js
+++ main.js
@@ -86,6 +86,33 @@ for(const person of array){
         tr.appendChild(tdMarried)
 }
 
+const form = document.getElementById("form")
+form.addEventListener('submit', function(e){
+        e.preventDefault()
+        const lastname = document.getElementById('lastname')
+        const firstname1 = document.getElementById('firstname1')
+        const firstname2 = document.getElementById('firstname2')
+        const married = document.getElementById('married')
+        const pet = document.getElementById('pet')
+
+        const lastnameValue = lastname.value;
+        const firstname1Value = firstname1.value;
+        const firstname2Value = firstname2.value;
+        const marriedValue = married.checked;
+        const petValue = pet.value;
+        
+        array.push (
+            {
+            lastname: lastnameValue,
+            firstname1: firstname1Value,
+            firstname2: firstname2Value,
+            married:marriedValue,
+            pet: petValue,
+        }
+        )
+
+})
+
 document.body.appendChild(table)
 table.appendChild(tableHeader)
 tableHeader.appendChild(tableHeaderRow)
